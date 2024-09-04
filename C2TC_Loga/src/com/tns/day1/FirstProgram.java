14ae1ef2ec8edb3c126b6c0b52664f818ebb759f
Logaranjani V <Logaranjani V@DESKTOP-H86LVQQ>
2024-09-04 12:19:20 +0530

    This is my first commit

---
+++ C2TC_Loga/src/com/tns/day1/FirstProgram.java
@@ -0,0 +1,20 @@
+package com.tns.day1;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		int num1=50;
+		int	num2=20;
+		int sum,sub,mul,div,mod;
+		sum=num1+num2;
+		System.out.println("sum:"+sum);
+		sub=num1-num2;
+		System.out.println("sub:"+sub);
+		mul=num1*num2;
+		System.out.println("mul:"+mul);
+		div=num1/num2;
+		System.out.println("div:"+div);
+		mod=num1%num2;
+		System.out.println("mod:"+mod);
+
+}
+}
