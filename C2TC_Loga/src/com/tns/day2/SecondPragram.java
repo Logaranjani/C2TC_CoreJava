22792f56167ad108baf8508cf1d10e22a2c02fb6
Logaranjani V <Logaranjani V@DESKTOP-H86LVQQ>
2024-09-05 12:28:38 +0530

    second commit

---
+++ C2TC_Loga/src/com/tns/day2/SecondPragram.java
@@ -0,0 +1,33 @@
+package com.tns.day2;
+
+public class SecondPragram {
+	
+	public static void main(String[] args) {
+
+			 System.out.println("Datatype\t Min.Value\t\t Max.Value\t"); 
+
+			 System.out.println("Byte\t\t"+ Byte.MIN_VALUE + "\t\t\t" + Byte.MAX_VALUE); 
+
+			 System.out.println("Short\t\t"+ Short.MIN_VALUE + "\t\t\t" + Short.MAX_VALUE); 
+
+	         System.out.println("Integer\t\t"+ Integer.MIN_VALUE + "\t\t" + Integer.MAX_VALUE); 
+
+	         System.out.println("Float\t\t"+ Float.MIN_VALUE + "\t\t\t" + Float.MAX_VALUE); 
+
+	         System.out.println("Long\t\t"+ Long.MIN_VALUE + "\t"+ Long.MAX_VALUE); 
+
+	         System.out.println("Double\t\t"+ Double.MIN_VALUE + "\t\t" + Short.MAX_VALUE); 
+
+	         System.out.println("Character\t"+ Character.MIN_VALUE + "\t\t" + Character.MAX_VALUE); 
+
+
+
+		}
+
+
+
+	}
+	
+
+
+
